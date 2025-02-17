c0dfd2d5187f51edfb9f9a22c10f4aefcb7d43a8
pranavn21 <pranavnair65@gmail.com>
2025-02-17 01:38:17 -0600

    works with 3-digit kaprekar constant (495) and 5-digit kaprekar pattern

--- kaprekars-constant/run.py
+++ kaprekars-constant/run.py
@@ -6,16 +6,22 @@ def getMinInteger(n: int) -> str:
 
 
 print("-------------------------------")
-x = input("Enter integer: ")
+x = int(input("Enter integer: "))
 count = 0
 print("-------------------------------" + "\n")
-while(x != 6174):
-    print("n = " + str(count) + ": " + str(x))
+while(x >= 100 and x <= 99999):
+    oldNum = x
     max = getMaxInteger(x)
     min = getMinInteger(x)
     diff = max - int(min)
+
+    if(diff == oldNum):
+        break
+
+    print("n = " + str(count) + ": " + str(x))
     print(str(max) + " - " + str(min) + " = " + str(diff))
     print("\n" + "-------------------------------" + "\n")
     x = diff
+
     count+=1
 
